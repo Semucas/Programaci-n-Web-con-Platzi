94987e70935237abda36117f10e35497f5841b45
Semucas <semucas27@gmail.com>
2026-03-02 13:15:18 -0500

    Muestra mensaje por selección de mascota

--- Mokepon/js/mokepon.js
+++ Mokepon/js/mokepon.js
@@ -1,6 +1,28 @@
-function seleccionarMascotaJugador() {
-    alert("SELECCIONASTE TU MASCOTA")
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById("boton-mascota")
+    botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById("boton-mascota")
-botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador)
+function seleccionarMascotaJugador() {
+
+    let inputHipodoge = document.getElementById("Hipodoge")
+    let inputCapipepo = document.getElementById("Capipepo")
+    let inputRatigueya = document.getElementById("Ratigueya")
+
+    if (inputHipodoge.checked) {
+        alert("Seleccionaste a Hipodoge")
+    }
+    else if (inputCapipepo.checked) {
+        alert("Seleccionaste a Capipepo")
+    }
+    else if (inputRatigueya.checked) {
+        alert("Seleccionaste a Ratigueya")
+    }
+    else {
+        alert("Selecciona alguna mascota")
+    }
+}
+
+
+
+window.addEventListener("load", iniciarJuego)
