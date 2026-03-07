e2dabe6c5a4992d5bd4d30a74b0761f3524950c7
Semucas <semucas27@gmail.com>
2026-03-07 11:49:40 -0500

    Mensaje dinámico con los ataques

--- Mokepon/js/mokepon.js
+++ Mokepon/js/mokepon.js
@@ -89,6 +89,16 @@ function ataqueAleatorioEnemigo() {
     else {
         ataqueEnemigo = "TIERRA"
     }
+    crearMensaje()
+}
+
+
+function crearMensaje() {
+    let sectionMensajes = document.getElementById("mensajes")
+
+    let parrafo = document.createElement("p")
+    parrafo.innerHTML = "Tu mascota atacó con " + ataqueJugador + " la mascota del enemigo atacó con " + ataqueEnemigo + "  Pendiente"
+    sectionMensajes.appendChild(parrafo)
 }
 
 
